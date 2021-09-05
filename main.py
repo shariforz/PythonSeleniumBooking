0bbd7ffe67252926ee25dea4537d3b250c5ad5a4
Sharif <orzikulov@gmail.com>
2021-09-06 00:45:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+from selenium import webdriver
+
+##os.environ['PATH'] = r"C:/SeleniumDrivers"
+driver = webdriver.Chrome()
+driver.get('https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html')
+my_element = driver.find_element_by_id("downloadButton")
+my_element.click()
